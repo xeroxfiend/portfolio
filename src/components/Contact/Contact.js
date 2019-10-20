ccf4e794cacd1f9b019d54f3600047889b39343b
Brian Lowe <mynameisboy@gmail.com>
2019-10-20 16:12:13 -0600

    styling

--- src/components/Contact/Contact.js
+++ src/components/Contact/Contact.js
@@ -3,9 +3,7 @@ import React from 'react'
 function Contact() {
     return(
         <div id='contact' className="contact-container">
-            <p className="contact">
                 <p className='contact-me'>Contact me!</p>
-            </p>
         </div>
     )
 }
