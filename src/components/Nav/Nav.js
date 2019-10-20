8efe87c0ad8a0ff46464345ebd098c48fb9046bc
Brian Lowe <mynameisboy@gmail.com>
2019-10-20 11:48:02 -0600

    smooth scrolling

--- src/components/Nav/Nav.js
+++ src/components/Nav/Nav.js
@@ -3,10 +3,10 @@ import './nav.css'
 
 function Nav() {
   return <div className="nav-container">
-      <a href="about" className="about">About</a>
-      <a href="skills" className="skills">Skills</a>
-      <a href="projects" className="projects">Projects</a>
-      <a href="contact" className="contact">Contact</a>
+      <a href="#about" className="about">About</a>
+      <a href="#skills" className="skills">Skills</a>
+      <a href="#projects" className="projects">Projects</a>
+      <a href="#contact" className="contact">Contact</a>
   </div>;
 }
 
