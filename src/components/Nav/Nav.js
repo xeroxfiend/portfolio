c5dbf29a5135e13d08d5cbe36a58911ae643bab7
Brian Lowe <mynameisboy@gmail.com>
2019-10-21 15:38:09 -0600

    kinda fixed artifacting issue

--- src/components/Nav/Nav.js
+++ src/components/Nav/Nav.js
@@ -1,12 +1,25 @@
 import React from "react";
 
 function Nav() {
-  return <div className="nav-container">
-      <a href="#about" className="about">About</a>
-      <a href="#skills" className="skills">Skills</a>
-      <a href="#projects" className="projects">Projects</a>
-      <a href="#contact" className="contact">Contact</a>
-  </div>;
+  return (
+    <div className="nav-container">
+      <header className="title">Brian Lowe</header>
+      <nav className="nav">
+        <a href="#about" className="about">
+          About
+        </a>
+        <a href="#skills" className="skills">
+          Skills
+        </a>
+        <a href="#projects" className="projects">
+          Projects
+        </a>
+        <a href="#contact" className="contact">
+          Contact
+        </a>
+      </nav>
+    </div>
+  );
 }
 
 export default Nav;
