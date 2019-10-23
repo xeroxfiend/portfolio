d5d659e66a4af9b4792645d8acdd30a9a34e8085
Brian Lowe <mynameisboy@gmail.com>
2019-10-23 14:22:10 -0600

    styling

--- src/components/Skills/Skills.js
+++ src/components/Skills/Skills.js
@@ -4,18 +4,66 @@ function Skills() {
   return (
     <div id="skills" className="skills-container">
       <div className="icons">
-        <i className="devicon-react-original colored"></i>
-        <i className="devicon-nodejs-plain colored"></i>
-        <i className="devicon-javascript-plain colored"></i>
-        <i className="devicon-express-original colored"></i>
-        <i className="devicon-typescript-plain colored"></i>
-        <i className="devicon-css3-plain-wordmark colored"></i>
-        <i className="devicon-git-plain colored"></i>
-        <i className="devicon-github-plain colored"></i>
-        <i className="devicon-html5-plain-wordmark colored"></i>
-        <i className="devicon-heroku-plain colored"></i>
-        <i className="devicon-postgresql-plain colored"></i>
-        <i className="devicon-sass-original colored"></i>
+        <div className="icon-container">
+          <i className="devicon-react-original colored">
+            <p className="title">React</p>
+          </i>
+        </div>
+        <div className="icon-container">
+          <i className="devicon-javascript-plain colored">
+            <p className="title">JavaScript</p>
+          </i>
+        </div>
+        <div className="icon-container">
+          <i className="devicon-nodejs-plain colored">
+            <p className="title">Node.js</p>
+          </i>
+        </div>
+        <div className="icon-container">
+          <i className="devicon-express-original colored">
+            <p className="title">Express</p>
+          </i>
+        </div>
+        <div className="icon-container">
+          <i className="devicon-typescript-plain colored">
+            <p className="title">TypeScript</p>
+          </i>
+        </div>
+        <div className="icon-container">
+          <i className="devicon-css3-plain colored">
+            <p className="title">CSS3</p>
+          </i>
+        </div>
+        <div className="icon-container">
+          <i className="devicon-git-plain colored">
+            <p className="title">Git</p>
+          </i>
+        </div>
+        <div className="icon-container">
+          <i className="devicon-github-plain colored">
+            <p className="title">GitHub</p>
+          </i>
+        </div>
+        <div className="icon-container">
+          <i className="devicon-html5-plain colored">
+            <p className="title">HTML5</p>
+          </i>
+        </div>
+        <div className="icon-container">
+          <i className="devicon-heroku-plain colored">
+            <p className="title">Heroku</p>
+          </i>
+        </div>
+        <div className="icon-container">
+          <i className="devicon-postgresql-plain colored">
+            <p className="title">PostgreSQL</p>
+          </i>
+        </div>
+        <div className="icon-container">
+          <i className="devicon-sass-original colored">
+            <p className="title">Sass</p>
+          </i>
+        </div>
       </div>
     </div>
   );
