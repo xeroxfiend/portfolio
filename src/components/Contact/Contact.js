402994cab267de92bbb9f8d79077b7f7dd30575a
Brian Lowe <mynameisboy@gmail.com>
2019-10-23 15:13:26 -0600

    some contact functionality

--- src/components/Contact/Contact.js
+++ src/components/Contact/Contact.js
@@ -3,7 +3,10 @@ import React from 'react'
 function Contact() {
     return(
         <div id='contact' className="contact-container">
-                <p className='contact-me'>Contact me!</p>
+                <header className='contact-me'>Contact me!</header>
+                <span onClick={() => window.open('https://github.com/xeroxfiend')}>GitHub: github.com/xeroxfiend</span>
+                <span onClick={() => window.open('mailto:brianchristopherlowe@gmail.com')}>Email: brianchristopherlowe@gmail.com</span>
+                <a href="tel:8014490173">Phone: (801) 449-0173</a>
         </div>
     )
 }
