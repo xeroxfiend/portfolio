ac23ea8f65fa59948cf83d0983dd86681a785d1f
Brian Lowe <mynameisboy@gmail.com>
2019-10-24 16:49:44 -0600

    cleaner formatted code

--- src/components/Contact/Contact.js
+++ src/components/Contact/Contact.js
@@ -1,15 +1,27 @@
-import React from 'react'
+import React from "react";
 
 function Contact() {
-    return(
-        <div id='contact' className="contact-container">
-                <header className='contact-me'>Contact me!</header>
-                <span onClick={() => window.open('https://github.com/xeroxfiend')}>GitHub: github.com/xeroxfiend</span>
-                <span onClick={() => window.open('mailto:brianchristopherlowe@gmail.com')}>Email: brianchristopherlowe@gmail.com</span>
-                <span onClick={() => window.open('https://www.linkedin.com/in/brian-christopher-lowe/')}>LinkedIn: linkedin.com/in/brian-christopher-lowe</span>
-                <a href="tel:8014490173">Phone: (801) 449-0173</a>
-        </div>
-    )
+  return (
+    <div id="contact" className="contact-container">
+      <header className="contact-me">Contact me!</header>
+      <span onClick={() => window.open("https://github.com/xeroxfiend")}>
+        GitHub: github.com/xeroxfiend
+      </span>
+      <span
+        onClick={() => window.open("mailto:brianchristopherlowe@gmail.com")}
+      >
+        Email: brianchristopherlowe@gmail.com
+      </span>
+      <span
+        onClick={() =>
+          window.open("https://www.linkedin.com/in/brian-christopher-lowe/")
+        }
+      >
+        LinkedIn: linkedin.com/in/brian-christopher-lowe
+      </span>
+      <a href="tel:8014490173">Phone: (801) 449-0173</a>
+    </div>
+  );
 }
 
-export default Contact
+export default Contact;
