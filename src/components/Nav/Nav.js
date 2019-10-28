f8af1ad2d02799c294b0e0547ed779c16a6eb79a
Brian Lowe <mynameisboy@gmail.com>
2019-10-27 18:15:25 -0600

    figuring out nav bar

--- src/components/Nav/Nav.js
+++ src/components/Nav/Nav.js
@@ -5,18 +5,46 @@ function Nav() {
     <div className="nav-container">
       <header className="title">BL</header>
       <nav className="nav">
-        <a href="#about" className="about">
-          A
-        </a>
-        <a href="#skills" className="skills">
-          S
-        </a>
-        <a href="#projects" className="projects">
-          P
-        </a>
-        <a href="#contact" className="contact">
-          C
-        </a>
+        <div className="about-icon">
+          <img
+            src="https://www.iconsdb.com/icons/preview/white/about-xxl.png"
+            alt=""
+            className="about-image"
+          />
+          <a href="#about" className="about">
+            A
+          </a>
+        </div>
+        <div className="skills-icon">
+        <img
+            src="https://www.iconsdb.com/icons/preview/white/about-xxl.png"
+            alt=""
+            className="skills-image"
+          />
+          <a href="#skills" className="skills">
+            S
+          </a>
+        </div>
+        <div className="projects-icon">  
+        <img
+            src="https://www.iconsdb.com/icons/preview/white/about-xxl.png"
+            alt=""
+            className="projects-image"
+          />
+          <a href="#projects" className="projects">
+            P
+          </a>
+        </div>
+        <div className="contact-icon">
+        <img
+            src="https://www.iconsdb.com/icons/preview/white/about-xxl.png"
+            alt=""
+            className="contact-image"
+          />
+          <a href="#contact" className="contact">
+            C
+          </a>
+        </div>
       </nav>
     </div>
   );
