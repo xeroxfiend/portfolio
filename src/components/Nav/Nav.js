1cc478f5cdfd75e5c5a1c309737a919ec326af91
Brian Lowe <mynameisboy@gmail.com>
2019-10-27 19:30:40 -0600

    styling!

--- src/components/Nav/Nav.js
+++ src/components/Nav/Nav.js
@@ -1,46 +1,35 @@
 import React from "react";
 
+import about from "../../assets/about.png";
+import skills from "../../assets/skills.png";
+import projects from "../../assets/projects.png";
+import contact from "../../assets/contact.png";
+
 function Nav() {
   return (
     <div className="nav-container">
       <header className="title">BL</header>
       <nav className="nav">
         <div className="about-icon">
-          <img
-            src="https://www.iconsdb.com/icons/preview/white/about-xxl.png"
-            alt=""
-            className="about-image"
-          />
+          <img src={about} alt="about" className="about-image" />
           <a href="#about" className="about">
             A
           </a>
         </div>
         <div className="skills-icon">
-        <img
-            src="https://www.iconsdb.com/icons/preview/white/about-xxl.png"
-            alt=""
-            className="skills-image"
-          />
+          <img src={skills} alt="skills" className="skills-image" />
           <a href="#skills" className="skills">
             S
           </a>
         </div>
-        <div className="projects-icon">  
-        <img
-            src="https://www.iconsdb.com/icons/preview/white/about-xxl.png"
-            alt=""
-            className="projects-image"
-          />
+        <div className="projects-icon">
+          <img src={projects} alt="projects" className="projects-image" />
           <a href="#projects" className="projects">
             P
           </a>
         </div>
         <div className="contact-icon">
-        <img
-            src="https://www.iconsdb.com/icons/preview/white/about-xxl.png"
-            alt=""
-            className="contact-image"
-          />
+          <img src={contact} alt="contact" className="contact-image" />
           <a href="#contact" className="contact">
             C
           </a>
