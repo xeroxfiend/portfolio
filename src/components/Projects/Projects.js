a8ba320992199d6bd4342359dfcb3a1724f87005
Brian Lowe <mynameisboy@gmail.com>
2019-11-19 11:43:46 -0700

    carousel is broken

--- src/components/Projects/Projects.js
+++ src/components/Projects/Projects.js
@@ -1,11 +1,50 @@
-import React from "react";
-import watchon from '../../assets/watchon.JPG'
+import React, {Component} from "react";
+// import watchon from "../../assets/watchon.JPG";
+import Slider from "react-slick";
 
-function Projects() {
-  return <div id='projects' className="projects-container">
-      <span className="projects-title-watchon">WatchOn</span>
-      <img onClick={() => window.open('http://watch-on.com')} src={watchon} alt="watch-on" className="watch-on"/>
-  </div>;
+class Projects extends Component {
+  render() {
+    const settings = {
+      dots: false,
+      infinite: true,
+      arrows: false,
+      speed: 500,
+      autoplay: true,
+      slidesToShow: 1,
+      slidesToScroll: 1,
+      fade:true,
+      cssEase: 'linear',
+      autoplaySpeed: 4000
+    };
+    return (
+      <div id="projects" className="projects-container">
+        <Slider {...settings}>
+          <div className="watch1"></div>
+          <div className="watch2"></div>
+          <div className="watch3"></div>
+        </Slider>
+        <Slider {...settings}>
+          <div className="smakd1"></div>
+          <div className="smakd2"></div>
+          <div className="smakd3"></div>
+          <div className="smakd4"></div>
+          <div className="smakd5"></div>
+          <div className="smakd6"></div>
+        </Slider>
+      </div>
+    );
+    // return (
+    //   <div id="projects" className="projects-container">
+    //     <span className="projects-title-watchon">WatchOn</span>
+    //     <img
+    //       onClick={() => window.open("http://watch-on.com")}
+    //       src={watchon}
+    //       alt="watch-on"
+    //       className="watch-on"
+    //     />
+    //   </div>
+    // );
+  }
 }
 
 export default Projects;
