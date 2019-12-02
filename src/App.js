23603f16909a30477d9cad81662ceef08f404ca8
Brian Lowe <mynameisboy@gmail.com>
2019-12-02 16:10:26 -0700

    change app structure

--- src/App.js
+++ src/App.js
@@ -8,13 +8,6 @@ import Projects from "./components/Projects/Projects";
 import Contact from "./components/Contact/Contact";
 
 class App extends Component {
-  constructor() {
-    super();
-    this.state = {
-      placeholder: ""
-    };
-  }
-
   render() {
     return (
       <div className="App">
