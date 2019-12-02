43dc402966282ea627bace0537b8e33c19975de0
Brian Lowe <mynameisboy@gmail.com>
2019-12-02 16:09:12 -0700

    add home component, headshot, and bio

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React, {Component} from "react";
 import "./App.scss";
 import Nav from "./components/Nav/Nav";
+import Home from "./components/Home/Home";
 import About from "./components/About/About";
 import Skills from "./components/Skills/Skills";
 import Projects from "./components/Projects/Projects";
@@ -19,6 +20,7 @@ class App extends Component {
       <div className="App">
         <Nav />
         <div className="sections">
+          <Home />
           <About />
           <Skills />
           <Projects />
--- src/components/About/About.js
+++ src/components/About/About.js
@@ -1,11 +1,28 @@
-import React from 'react'
+import React from "react";
+import headshot from '../../assets/headshot.jpg'
 
 function About() {
-    return(
-        <div id='about' className="about-container">
-            <p className="bio">Hi! I'm Brian!</p>
-        </div>
-    )
+  return (
+    <div id="about" className="about-container">
+      <p className="bio">
+        Hello, my name is Brian! You can say that I’m one of those classic
+        examples of people who go to college and jump around with their majors,
+        trying to find a career path that sticks. I explored music and
+        astronomy, dabbled in computer science, and excelled in math. However,
+        nothing I studied compelled me enough to continue exploring until I
+        discovered web development. Once I immersed myself in the work, I knew
+        immediately that this was my calling. My years of QA experience prepared
+        me to be precise, meticulous, and creative in the coding process. I love
+        being presented with complicated problems, and I find flow in digging in
+        my toes to find the solution, no matter how long it takes. I have a
+        passion for building clean, beautifully styled, and efficient apps. When
+        I’m not coding as a hobby (seriously), my other passions include
+        audiobooks & puzzles with my wife, rock climbing, and all things Star
+        Trek. If you have any questions, please don’t hesitate to contact me!
+      </p>
+      <img src={headshot} alt='headshot' className="head-shot"/>
+    </div>
+  );
 }
 
-export default About
+export default About;
